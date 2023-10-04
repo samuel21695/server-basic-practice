510d4eb37378237027458aafeeeae182161df220
samuel21695 <samuel21695@gmail.com>
2023-10-04 16:20:10 +0900

    writeHead

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-console.log('hi')
+const http = require('node:http');
+const fs = require('fs');
+
+http.createServer(function(request, response) {
+  let writeHeadObject = {
+    'Content-Type': 'text/html'
+  }
+  response.writeHead(200, writeHeadObject)
+})
