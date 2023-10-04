b1970cec46253105a0ed41c9a9180470e864fcd0
samuel21695 <samuel21695@gmail.com>
2023-10-04 16:24:52 +0900

    fs.readFile

--- app.js
+++ app.js
@@ -8,4 +8,11 @@ http.createServer(function(request, response) {
     'Content-Type': 'text/html'
   }
   response.writeHead(200, writeHeadObject)
-})
+  fs.readFile("./index.html", function(err, data) {
+    if (err) {
+      console.error('파일을 읽지 못했습니다.')
+    } else {
+      response.end(data);
+    }
+  }) 
+}).listen(8080)
